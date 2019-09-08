25809818699ceecfe6ae65bb6efe29047e9391c8
Denis Misiurca <infoman1985@gmail.com>
2019-09-09 01:12:11 +0300

    Don't use session setup in user controller spec
    (as it's handled by devise helpers)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,16 +24,6 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe UsersController, type: :controller do
-
-  # This should return the minimal set of attributes required to create a valid
-  # User. As you add validations to User, be sure to
-  # adjust the attributes here as well.
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # UsersController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   before :each do
     @user = create :user
   end
